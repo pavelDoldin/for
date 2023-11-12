5e100a8d2bc06d6d7ce50a448e15bdde21757aa6
pavelDoldin <114822963+pavelDoldin@users.noreply.github.com>
2023-11-12 07:50:38 +0500

    add_ex14

--- src/For/For.java
+++ src/For/For.java
@@ -8,19 +8,31 @@ import java.util.Random;
 public class For {
     public static void main(String[] args) {
 
-
-        ex13();
+        System.out.println(ex14());
     }
-    static void ex13(){
+    static boolean ex14(){
+
+        // проверить слово на полиндром
+        String str = "потопf";
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void ex13() {
 
         /*
          написать пузырьковый в цикле while
          */
-    Random random = new Random();
+        Random random = new Random();
 
-    int syez = random.nextInt(3,10);
+        int syez = random.nextInt(3, 10);
 
-    int[] arr = new int[syez];
+        int[] arr = new int[syez];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(0, 11);
         }
@@ -30,10 +42,10 @@ public class For {
 
         int temp;
         boolean sort = false;
-        while (!sort){
+        while (!sort) {
             sort = true;
             for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]){
+                if (arr[i] > arr[i + 1]) {
                     temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
@@ -44,7 +56,8 @@ public class For {
         System.out.println(Arrays.toString(arr));
 
     }
-    static void ex12(){
+
+    static void ex12() {
         /*
         Написать пузырьковый метод сортировки
          */
@@ -55,7 +68,7 @@ public class For {
         int[] arr = new int[seyz];
 
         for (int i = 0; i < seyz; i++) {
-            arr[i] = random.nextInt(0,10);
+            arr[i] = random.nextInt(0, 10);
         }
         System.out.println(Arrays.toString(arr));
 
@@ -65,17 +78,18 @@ public class For {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] > arr[j + 1]){
+                if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j +1] = temp;
+                    arr[j + 1] = temp;
                 }
             }
         }
         System.out.println(Arrays.toString(arr));
 
     }
-    static void ex11(){
+
+    static void ex11() {
 
         // Поработать и ArrayList
         ArrayList<Integer> integerArrayList = new ArrayList<>();
@@ -87,23 +101,21 @@ public class For {
         arrayList.add(123);
 
 
-
         System.out.println(arrayList);
         System.out.println(arrayList.size()); // Выведет количество элементов в списке
         System.out.println(arrayList.get(2)); // Выведет по индексу значение
         System.out.println();
-        arrayList.set(0,1);                   // Добавит элемент под индексом
+        arrayList.set(0, 1);                   // Добавит элемент под индексом
         //arrayList.remove(2);            // Удалит по индексу
 
         System.out.println(arrayList);
 
-        System.out.println(arrayList.subList(0,1));
-
-
+        System.out.println(arrayList.subList(0, 1));
 
 
     }
-    static void ex10(){
+
+    static void ex10() {
 
         /*
         Дан мфссив nums = [3, 2, 5, 3] и число val = 3 и тог: [2, 5, 3, 3]
@@ -113,12 +125,9 @@ public class For {
          */
         Random random = new Random();
 
-        int syiz = random.nextInt(5,21);
+        int syiz = random.nextInt(5, 21);
         int[] nums = new int[syiz];
         int val = 3;
-
-
-
 
         for (int i = 0; i < nums.length; i++) {
             nums[i] = random.nextInt(5);
@@ -127,24 +136,8 @@ public class For {
         System.out.println(Arrays.toString(nums));
 
 
-        //for (int i = 0, j = nums.length - 1; i < nums.length; i++) {
-        //    int temp;
-        //    if (nums[i] != val){
-        //        i++;
-        //    } else {
-        //        temp = nums[j];               Доработать
-        //        nums[j] = nums[i];
-        //        nums[i] = temp;
-        //        j--;
-        //    }
-//
-        //}
-        //System.out.println(Arrays.toString(nums));
-
-
         int[] res = new int[nums.length];
         Arrays.fill(res, val);
-
 
         for (int i = 0, j = 0; i < nums.length; i++) {
                 if (nums[i] != val){
@@ -156,39 +149,40 @@ public class For {
         System.out.println(Arrays.toString(res));
 
     }
-    static void ex9(){
-    //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
+
+    static void ex9() {
+        //Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
 
         Random random = new Random();
 
         int count = 0;
         int countMax = 0;
         int a = 1;
-        int size = random.nextInt(5,15);
+        int size = random.nextInt(5, 15);
         int[] arr = new int[size];
         System.out.println(size);
 
         for (int i = 0; i < arr.length; i++) {
-           arr[i] = random.nextInt(2);
-           if (arr[i] == a){
-               count++;
-           }
-           else {
-               if (count > countMax){
-                   countMax = count;
-               }
-               count = 0;
-           }
+            arr[i] = random.nextInt(2);
+            if (arr[i] == a) {
+                count++;
+            } else {
+                if (count > countMax) {
+                    countMax = count;
+                }
+                count = 0;
+            }
 
         }
-        if (count > countMax){
+        if (count > countMax) {
             countMax = count;
         }
 
         System.out.println(Arrays.toString(arr));
         System.out.println(countMax);
     }
-    static void ex8(){
+
+    static void ex8() {
         /*
         Напишите программу, которая генерирует 25 случайных чисел типа int,
         для каждого значения команда if - else сообщает, в каком отношении оно находится
@@ -196,8 +190,8 @@ public class For {
          */
         Random random = new Random();
 
-        int saiz = random.nextInt(1,25);
-        int [] array = new int[saiz];
+        int saiz = random.nextInt(1, 25);
+        int[] array = new int[saiz];
 
         System.out.println(saiz);
 
@@ -209,48 +203,50 @@ public class For {
 
         System.out.println(Arrays.toString(array));
 
-        for (int i = 0; i < array.length-1 ; i++) {
-            if (array[i] > array[i + 1]){
-                System.out.println(array[i] + " > " + array[i +1]);
-            }
-            else if (array[i] < array[i + 1]){
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                System.out.println(array[i] + " > " + array[i + 1]);
+            } else if (array[i] < array[i + 1]) {
                 System.out.println(array[i] + " < " + array[i + 1]);
-            }
-            else {
-                System.out.println(array[i] + " = " + array[i +1]);
+            } else {
+                System.out.println(array[i] + " = " + array[i + 1]);
             }
         }
     }
-    static void ex7(){
+
+    static void ex7() {
         //Объявляются 2 целочисленных переменных и им присваиваются значения.
         // Вывести на экран в интервале от меньшего к большему все чётные числа.
 
         int i = 5;
         int count = 15;
 
-        for (; i <=count ; i++) {
-            if (i % 2 == 0){
+        for (; i <= count; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
     }
-    static void ex6(){
+
+    static void ex6() {
         //Вывести на экран числа от 5 до 95 включительно с интервалом 10
 
         int count = 95;
-        for (int i = 5; i <= count ; i+=10) {
+        for (int i = 5; i <= count; i += 10) {
             System.out.println(i);
         }
     }
-    static void ex5(){
+
+    static void ex5() {
         // Вывести на экран числа от 0 до 30 включительно с интервалом 5
 
         int count = 30;
-        for (int i = 0; i <= count; i +=5) {
+        for (int i = 0; i <= count; i += 5) {
             System.out.println(i);
         }
     }
-    static void ex4(){
+
+    static void ex4() {
         // Найти факториал n - числа
 
         int count = 5;
@@ -260,7 +256,8 @@ public class For {
         }
         System.out.println(res);
     }
-    static void ex3(){
+
+    static void ex3() {
         /*
         Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует
         все числа от 1 до введенного пользователем числа.
@@ -277,7 +274,8 @@ public class For {
         }
         System.out.println(sum);
     }
-    static void ex2(){
+
+    static void ex2() {
         //Необходимо вывести на экран таблицу умножения на 3:
         //В столбик
         //3*1=3 ; 3*2=6 ; 3*3=9 ; 3*4=12 ; 3*5=15 ; 3*6=18
@@ -285,17 +283,19 @@ public class For {
 
         int count = 10;
         for (int i = 1; i <= count; i++) {
-            System.out.println("3 * " + i + " =" + 3*i);
+            System.out.println("3 * " + i + " =" + 3 * i);
         }
     }
-    static void ex1(){
+
+    static void ex1() {
         //Необходимо вывести на экран числа от 5 до 1.
         //На экране должно быть: 5 4 3 2 1
-        for (int i = 5; i >= 1 ; i--) {
+        for (int i = 5; i >= 1; i--) {
             System.out.println(i);
         }
     }
-    static void ex0(){
+
+    static void ex0() {
         //Необходимо вывести на экран числа от 1 до 5.
         //На экране должно быть: 1 2 3 4 5
         int count = 5;
