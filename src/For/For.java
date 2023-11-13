c2096bafb6e2e882258b36cfc1ba49bac8cdc9ef
pavelDoldin <114822963+pavelDoldin@users.noreply.github.com>
2023-11-13 17:39:07 +0500

    add_ex19

--- src/For/For.java
+++ src/For/For.java
@@ -6,8 +6,38 @@ import java.util.*;
 public class For {
     public static void main(String[] args) {
 
-        ex18();
+        ex19();
     }
+
+    static void ex19() {
+        /*
+        Создать структуру для хранения Номеров паспортов и Фамилий сотрудников организации.
+        123456 Иванов
+        321456 Васильев
+        234561 Петрова
+        234432 Иванов
+        654321 Петрова
+        345678 Иванов
+        Вывести данные по сотрудникам с фамилией Иванов.
+         */
+
+        Map<String, String> map = new HashMap<>();
+        map.put("123456", "Иванов");
+        map.put("321456", "Васильев");
+        map.put("234561", "Петрова");
+        map.put("234432", "Иванов");
+        map.put("654321", "Петрова");
+        map.put("345678", "Иванов");
+
+        String name = "Петрова";
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getValue().equals(name)){
+                System.out.println(entry.getKey() + " = " + entry.getValue());
+            }
+        }
+    }
+
 
     static void ex18() {
         /*
@@ -20,7 +50,6 @@ public class For {
         int[] arr = new int[size];
 
         int sum = 0;        // сумма чисел
-
 
 
         for (int i = 0; i < arr.length; i++) {
