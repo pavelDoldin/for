eda4e0a6ab078bc0c7c472dc8f7d54659c3d7136
pavelDoldin <114822963+pavelDoldin@users.noreply.github.com>
2023-11-21 16:50:03 +0500

    add_Task11

---
+++ src/ArrayList/Task11.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Task11 {
+    public void task11(){
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+
+        //Collections.reverse(list);
+
+
+        System.out.println(list);
+    }
+}
